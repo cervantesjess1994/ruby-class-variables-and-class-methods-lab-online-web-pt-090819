022f9e53069babe17285a66ed43c3980de8bccd5
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-02 00:58:30 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+
+  @@count = 0
+  @@genre = []
+  @@artists = []
+  @@genre_count
+  @@artist_count
+
+  attr_reader :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@genre[] << "#{genre}"
+    @@artists[] << "#{artist}"
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genre
+    @@genre = []
+  end
+
+  def self.artists
+    @@artists = []
+  end
+
+  def self.genre_count(genre, num_of_songs)
+    @@genre_count = {genre: num_of_songs}
+#    @@genre_count.array.any?("#{genre}")
+#      if genre_exists
+  #      genre +1
+    #  else create_genre_and_add_to_array
+
+    end
+
+  end
+
+  def self.artist_count(artist, name_of_song)
+    @@artist_count = {artist: name_of_song}
+  end
+end
