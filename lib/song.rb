bd2d881fc6f1ae217106e79b0343394ec1769aea
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-02 01:10:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,15 @@ class Song
   end
 
   def self.genre_count
-    @@genres.inject(Hash.new(0)) {|total,i| total[i] += 1 ; total}
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
 
   def self.artist_count(artist, name_of_song)
